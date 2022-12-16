c66905aed956bab0051564093a42ac95c09636a5
Yu Qin <sirius.qinyu@gmail.com>
2022-12-16 13:50:12 +0800

    create a express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("123");
+import express from "express";
+import { Express } from "express-serve-static-core";
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello world!!!");
+});
+
+server.listen(3000, () => {
+    console.info(`Listening on http://localhost:3000`);
+});
